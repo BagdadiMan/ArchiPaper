88ac61c1acab9cd16d0f8a53fb8372c6d47a0745
BagdadiMan <nimroded99@gmail.com>
2021-06-06 15:41:56 +0300

    CommonProblems

---
+++ src/Pages/CommonProblems/CommonProblems.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Accordion, AccordionSummary, AccordionDetails, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+    },
+    heading: {
+      fontSize: theme.typography.pxToRem(17),
+      fontWeight: theme.typography.fontWeightRegular,
+    },
+  }));
+
+const CommonProblems = (props) => { 
+    const classes = useStyles();
+
+    const {problems} = props;
+
+    return (
+        <div className={classes.root}>
+            {problems && problems.map(x => <Accordion key={x.title}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                >
+                    <Typography className={classes.heading}>{x.title}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>{x.content}</Typography>
+                </AccordionDetails>
+            </Accordion>)}
+        </div>
+    );
+}
+
+
+export default CommonProblems;
